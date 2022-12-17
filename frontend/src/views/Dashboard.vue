7e6a56828175cd1cb93cebdc75e1619869081607
Eduard <ea@rostkov.me>
2022-12-17 09:50:03 +0400

    Интерфейс для работы

--- frontend/src/views/Dashboard.vue
+++ frontend/src/views/Dashboard.vue
@@ -2,7 +2,9 @@
   <div>
     <CRow>
       <CCol md="12">
-
+        <Ontologies></Ontologies>
+        <NewOntologyFill></NewOntologyFill>
+        <OntologiesTasks></OntologiesTasks>
       </CCol>
     </CRow>
   </div>
@@ -10,7 +12,12 @@
 
 <script>
 
+import Ontologies from "@/views/ontologies/Ontologies.vue";
+import OntologiesTasks from "@/views/ontologies/OntologiesTasks.vue";
+import NewOntologyFill from "@/views/ontologies/NewOntologyFill.vue";
+
 export default {
   name: 'Dashboard',
+  components: {Ontologies, NewOntologyFill, OntologiesTasks},
 }
 </script>
---
+++ frontend/src/views/ontologies/NewOntologyFill.vue
@@ -0,0 +1,45 @@
+<template>
+  <CContainer>
+    <CRow>
+      <CCol md="12">
+        <CCard>
+          <CCardHeader>
+            Импортировать
+          </CCardHeader>
+          <CCardBody>
+            <CRow>
+              <CCol sm="6">
+                <CInputFile :value.sync="input.owl" label="OWL" placeholder="Исходная онтология"/>
+              </CCol>
+              <CCol sm="6">
+                <CInputFile :value.sync="input.owl" label="Факты" placeholder="Факты для наполнения"/>
+              </CCol>
+            </CRow>
+            <CButton key="send" color="success" class="m-2" @click="send">Начать</CButton>
+          </CCardBody>
+        </CCard>
+      </CCol>
+    </CRow>
+  </CContainer>
+</template>
+
+<script>
+import Action from "@/services/Action";
+
+export default {
+  name: "NewOntologyFill",
+  data() {
+    return {
+      input: {
+        owl: "",
+        facts: ""
+      }
+    }
+  },
+  methods: {
+    send() {
+      Action.formAction("fill_ontology", this.input);
+    },
+  },
+}
+</script>
