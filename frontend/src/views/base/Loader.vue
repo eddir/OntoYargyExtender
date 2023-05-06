cb2cde1d787758a7261cba05e949cbc1a0d57438
Eduard <ea@rostkov.me>
2023-05-07 02:17:24 +0400

    add: компонент Loader

---
+++ frontend/src/views/base/Loader.vue
@@ -0,0 +1,39 @@
+<template>
+  <CCol md="12" ref="loader" hidden>
+    <CContainer>
+      <CRow>
+        <CCol md="12">
+          <CCard>
+            <CCardHeader>
+              Заполнение онтологий
+            </CCardHeader>
+            <CCardBody>
+              <CSpinner
+                  color="primary"
+                  style="width:4rem;height:4rem;"
+              />
+            </CCardBody>
+          </CCard>
+        </CCol>
+      </CRow>
+    </CContainer>
+  </CCol>
+</template>
+
+<script>
+export default {
+  name: "Loader",
+  methods: {
+    hide() {
+      this.$refs.loader.hidden = true;
+    },
+    show() {
+      this.$refs.loader.hidden = false;
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- frontend/src/views/filler/Filler.vue
+++ frontend/src/views/filler/Filler.vue
@@ -4,35 +4,18 @@
       <CCol ref="filler" md="12">
         <NewOntologyFill @update="start"></NewOntologyFill>
       </CCol>
-      <CCol ref="wip" md="12" hidden>
-        <CContainer>
-          <CRow>
-            <CCol md="12">
-              <CCard>
-                <CCardHeader>
-                  Заполнение онтологий
-                </CCardHeader>
-                <CCardBody>
-                  <CSpinner
-                      color="primary"
-                      style="width:4rem;height:4rem;"
-                  />
-                </CCardBody>
-              </CCard>
-            </CCol>
-          </CRow>
-        </CContainer>
-      </CCol>
+      <Loader ref="wip"></Loader>
     </CRow>
   </div>
 </template>
 <script>
 import NewOntologyFill from "@/views/ontologies/NewOntologyFill.vue";
 import API from "@/services/API.vue";
+import Loader from "@/views/base/Loader.vue";
 
 export default {
   name: 'Filler',
-  components: {NewOntologyFill},
+  components: {NewOntologyFill, Loader},
   data() {
     return {
       channel: null,
@@ -51,11 +34,11 @@ export default {
   methods: {
     start() {
       this.$refs.filler.hidden = true;
-      this.$refs.wip.hidden = false;
+      this.$refs.wip.show();
     },
     finish(ontology_id) {
       this.$refs.filler.hidden = false;
-      this.$refs.wip.hidden = true;
+      this.$refs.wip.hide();
       this.$toast.success("Онтология заполнена");
       API.downloadFilledOntology(ontology_id)
     },
